801e3873f18dc694fd7ae0c2e0cde4f3ca903ec2
Nicola <nicolamelito.11@gmail.com>
2020-11-04 17:32:53 +0100

    lo stesso implemento effettuato su 'Conti'
    aggiornato anche con lista come riportato da Ale in comunicazioni

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
   $('.btn').click(function() {
-  
-
     var dis = $('.bar')
     dis.toggle('active')
-
-
-
   })
+})
+
+
+$('.btn-due').click(function(){
+  var disDue = $('.second-menu')
+  disDue.toggle('active')
 })
