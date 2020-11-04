747a76429d2d35473d3cad2afaf443354e3b765e
Nicola <nicolamelito.11@gmail.com>
2020-11-04 18:37:47 +0100

    dropdown implementato su tutti
    manca stile grafico

--- js/script.js
+++ js/script.js
@@ -4,10 +4,32 @@ $(document).ready(function(){
     var dis = $('.bar')
     dis.toggle('active')
   })
-})
 
 
 $('.btn-due').click(function(){
   var disDue = $('.second-menu')
   disDue.toggle('active')
+})
+
+
+$('.btn-tre').click(function(){
+  var disTre = $('.terz-menu')
+  disTre.toggle('active')
+})
+
+$('.btn-quattro').click(function(){
+  var disQuat = $('.quart-menu')
+  disQuat.toggle('active')
+})
+
+
+  $('.btn-cinque').click(function(){
+    var disCiq = $('.quint-menu')
+    disCiq.toggle('active')
+  })
+
+  $('.btn-sei').click(function(){
+    var disSei = $('.sei-menu')
+    disSei.toggle('active')
+  })
 })
